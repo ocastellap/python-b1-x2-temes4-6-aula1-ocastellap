4b6d8c87f9c940ce10d2070278c29f089f7fe87f
ocastellap <ocastellap@uoc.edu>
2026-01-17 20:18:34 +0100

    Add imports and __all__ definition in __init__.py

--- bills/__init__.py
+++ bills/__init__.py
@@ -1,2 +1,11 @@
 # Write your imports here
+from .entity import Person, Buyer, Seller
+from .item import ISD_FACTOR, TaxType, Tax, Product, Bill
+from .stats import OrderType, Statistics
+
+__all__ = [
+    "Person", "Buyer", "Seller",
+    "ISD_FACTOR", "TaxType", "Tax", "Product", "Bill",
+    "OrderType", "Statistics"
+]
 
