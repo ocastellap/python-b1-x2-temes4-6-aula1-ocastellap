98b020bc409639e92f15d70fe472a20d85bddc57
ocastellap <ocastellap@uoc.edu>
2026-01-17 20:19:22 +0100

    Refactor Person class to be abstract and add Seller

--- bills/entity.py
+++ bills/entity.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 
-class Person:
+class Person(ABC):
     def __init__(self, dni: str, email: str, mobile: str):
         # Write here your code
+        self.dni = dni
+        self.email = email
+        self.mobile = mobile
         pass
 
     @abstractmethod
@@ -20,6 +23,11 @@ class Person:
 class Buyer(Person):
     def __init__(self, dni: str, email: str, mobile: str, full_name: str, age: int, address: str):
         # Write here your code
+        super().__init__(dni, email, mobile)
+        self.full_name = full_name
+        self.age = age
+        self.address = address
+
         pass
 
     def print(self):
@@ -28,8 +36,11 @@ class Buyer(Person):
 
 class Seller(Person):
     # Write the parameters in the next line
-    def __init__():
-        # Write here your code        
+    def __init__(self, dni: str, email: str, mobile: str, bussines_name: str, bussines_address: str):
+        # Write here your code
+        super().__init__(dni, email, mobile)
+        self.bussines_name = bussines_name
+        self.bussines_address = bussines_address
         pass
         
     def print(self):
